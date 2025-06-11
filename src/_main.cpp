b7105f84506a5f3b0f72877cc384d550cf31b318
Tobias B. <t-l-b97@gmx.de>
2025-06-11 17:03:23 +0200

    Tested BT connection between eps32 and android, example is working

---
+++ src/_main.cpp
@@ -0,0 +1,360 @@
+#include <Arduino.h>
+#include "settings.h"
+
+#include <SimpleCLI.h>
+
+#include "BTS7960.h"
+
+#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
+
+#include <ArduinoOSCWiFi.h>
+
+#include <ESP_DoubleResetDetector.h>      //https://github.com/khoih-prog/ESP_DoubleResetDetector
+// Number of seconds after reset during which a 
+// subseqent reset will be considered a double reset.
+
+DoubleResetDetector* drd;
+bool DRD_Detected = false;
+
+BTS7960 fanController(D_OUT_ENABLE_FAN, PWM_OUT_LPWM, PWM_OUT_RPWM);
+
+SimpleCLI cli;
+Command cmdSpeed;
+Command cmdDirection;
+Command cmdSimulate;
+Command cmdstopFans;
+bool simulating = true;
+bool stopFans = true;
+int counter = 0;
+unsigned long lastMessage = 0;
+unsigned long lastResponse = 0;
+
+unsigned loopDelta = 0;
+unsigned long lastMillis = 0;
+
+void turn(BTS7960::Direction direction, uint8_t speed)
+{
+    fanController.Turn(direction, speed);
+    /*Serial.print("Direction: ");
+    Serial.print((int) fanController.getDirection());
+    Serial.print(" Speed: ");
+    Serial.print(fanController.getPwmValue());
+    Serial.print(" Simulate: ");
+    Serial.println(simulating);*/
+}
+void statusinfo(){
+    Serial.print(" Direction: ");
+    Serial.print((int) fanController.getDirection());
+    Serial.print(" Speed: ");
+    Serial.print(fanController.getPwmValue());
+    Serial.print(" Simulate: ");
+    Serial.print(simulating);
+    Serial.print(" Stop Fans: ");
+    Serial.println(stopFans);
+}
+
+void wifi_setup()
+{
+    // WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
+    // it is a good practice to make sure your code sets wifi mode how you want it.
+
+    // put your setup code here, to run once:
+    
+    // pinMode(BTN_WIFI_RESET, INPUT_PULLUP);
+    delay(100);
+    int countDown = 10 * 1000;
+    //WiFiManager, Local intialization. Once its business is done, there is no need to keep it around
+    WiFiManager wm;
+    wm.setHostname(hostName.c_str());
+    if(DRD_Detected)
+    {
+        wm.resetSettings();
+        delay(100);
+    }
+
+    bool res;
+    Serial.println("AutoConnect");
+    res = wm.autoConnect(configApSSID.c_str(), configApPW.c_str()); // password protected ap
+    //res = wm.autoConnect("RATATATATA", "12345678");
+    // reset settings - wipe stored credentials for testing
+    // these are stored by the esp library
+    // wm.resetSettings();
+
+    // Automatically connect using saved credentials,
+    // if connection fails, it starts an access point with the specified name ( "AutoConnectAP"),
+    // if empty will auto generate SSID, if password is blank it will be anonymous AP (wm.autoConnect())
+    // then goes into a blocking loop awaiting configuration and will return success result
+
+
+    if(!res) {
+        Serial.println("Failed to connect");
+        // ESP.restart();
+    } 
+    else {
+        //if you get here you have connected to the WiFi    
+        Serial.println("connected...yeey :)");
+    }
+    Serial.println();
+}
+
+void cliErrorCallback(cmd_error* errorPtr) {
+    CommandError e(errorPtr);
+
+    Serial.println("ERROR: " + e.toString());
+
+    if (e.hasCommand()) {
+        Serial.println("Did you mean? " + e.getCommand().toString());
+    } else {
+        Serial.println(cli.toString());
+    }
+}
+
+void cliSpeedCallback(cmd* cmdPtr) {
+    Command cmd(cmdPtr);
+
+    Argument argSpeed = cmd.getArgument("value");
+    if(argSpeed.isSet())
+    {
+        String speedString = argSpeed.getValue();
+        int speed = speedString.toInt();
+        if(speed >= 0 && speed < 256)
+        {
+            turn(fanController.getDirection(), speed);
+        }
+    }
+}
+
+void cliDirectionCallback(cmd* cmdPtr) {
+    Command cmd(cmdPtr);
+
+    Argument argDirection   = cmd.getArgument("value");
+    if(argDirection.isSet())
+    {
+        String directionString = argDirection.getValue();
+        int direction          = directionString.toInt();
+        if(direction >= 0 && direction < 3)
+        {
+            turn((BTS7960::Direction) direction, fanController.getPwmValue());
+        }
+    }
+}
+
+void cliSimulationCallback(cmd* cmdPtr) {
+    Command cmd(cmdPtr);
+
+    Argument argSimulation   = cmd.getArgument("value");
+    if(argSimulation.isSet())
+    {
+        String simulateString = argSimulation.getValue();
+        int simulate          = simulateString.toInt(); 
+        if(simulate >= 0 && simulate < 2)
+        {
+            simulating = simulate;
+        }
+    }
+}
+void clistopFansCallback(cmd* cmdPtr) {
+    Command cmd(cmdPtr);
+
+    Argument argSimulation   = cmd.getArgument("value");
+    if(argSimulation.isSet())
+    {
+        String stopFansString = argSimulation.getValue();
+        int fanStop          = stopFansString.toInt(); 
+        if(fanStop >= 0 && fanStop < 2)
+        {
+            stopFans = fanStop;
+        }
+    }
+}
+
+void cli_loop()
+{
+    if (Serial.available())
+    {
+        String input = Serial.readStringUntil('\n');
+        Serial.print("# ");
+        Serial.println(input);
+        cli.parse(input);
+    }
+}
+
+void cli_setup()
+{
+    cmdSpeed = cli.addCmd("s/peed", cliSpeedCallback);
+    cmdSpeed.addPosArg("value");
+
+    cmdDirection = cli.addCmd("d/irection", cliDirectionCallback);
+    cmdDirection.addPosArg("value");
+
+    cmdSimulate = cli.addCmd("s/imulate", cliSimulationCallback);
+    cmdSimulate.addPosArg("value");
+
+    cmdstopFans = cli.addCmd("s/topFans", clistopFansCallback);
+    cmdstopFans.addPosArg("value");
+  
+    cli.setOnError(cliErrorCallback);
+
+}
+
+void oscReply(const String &remoteAddress)
+{
+    Serial.print(remoteAddress);
+    statusinfo();
+    lastMessage = millis();
+    //OscWiFi.send(remoteAddress.c_str(), OSC_SEND_PORT, "/fan/speed/state", (int)fanController.getPwmValue());
+    //OscWiFi.send(remoteAddress.c_str(), OSC_SEND_PORT, "/fan/direction/state", (int)fanController.getDirection());
+    OscWiFi.send(remoteAddress.c_str(), OSC_SEND_PORT, "/fan/connected/state", true);
+}
+
+void oscSpeedCallback(const OscMessage& m)
+{
+    if(m.isInt32(0))
+    {
+        int val = m.arg<int>(0);
+        if(val >= 0 && val < 256)
+        {
+            turn(fanController.getDirection(), val);
+        }
+        oscReply(m.remoteIP());   
+    }
+}
+
+void oscDirectionCallback(const OscMessage& m)
+{
+    if(m.isInt32(0))
+    {
+        int val = m.arg<int>(0);
+        if(val >= 0 && val < 3)
+        {
+            turn((BTS7960::Direction)val, fanController.getPwmValue());
+        }
+        oscReply(m.remoteIP());
+    }
+}
+void oscSimulationCallback(const OscMessage& m)
+{
+    if(m.isInt32(0))
+    {
+        int val = m.arg<int>(0);
+        if(val >= 0 && val < 2)
+        {
+            simulating = val;
+        }
+        oscReply(m.remoteIP());
+    }
+}
+void oscStopFansCallback(const OscMessage& m)
+{
+    if(m.isInt32(0))
+    {
+        int val = m.arg<int>(0);
+        if(val >= 0 && val < 2)
+        {
+            stopFans = val;
+        }
+        oscReply(m.remoteIP());
+    }
+}
+
+void osc_setup()
+{
+    OscWiFi.subscribe(OSC_LISTENER_PORT, "/fan/speed/set", oscSpeedCallback);
+    OscWiFi.subscribe(OSC_LISTENER_PORT, "/fan/direction/set", oscDirectionCallback);
+    OscWiFi.subscribe(OSC_LISTENER_PORT, "/fan/simulate/set", oscSimulationCallback);
+    OscWiFi.subscribe(OSC_LISTENER_PORT, "/fan/stopFans/set", oscStopFansCallback);
+}
+
+void setup_deletetowork() {
+    Serial.begin(9600);
+
+    drd = new DoubleResetDetector(DRD_TIMEOUT, DRD_ADDRESS);
+    if (drd->detectDoubleReset()) 
+    {
+        Serial.println("Double Reset Detected");
+        DRD_Detected = true;
+    } 
+
+    // put your setup code here, to run once:
+    // pinMode(BTN_WIFI_RESET, INPUT_PULLUP);
+    pinMode(LED_BUILTIN, OUTPUT);
+    //pinMode(A_IN_RPWM, INPUT_PULLDOWN);
+    Serial.println("I am " + hostName);
+    cli_setup();
+    #ifdef USE_WIFI
+      wifi_setup();
+      osc_setup();
+    #endif
+    Serial.println("Setup done");
+    lastMillis = millis();
+    lastResponse = millis();
+}
+
+float sinusCurveforSimulating(float y){
+    float x;
+    x = sin(y*20)/16 + sin(y*25+PI)/20 + sin(y*28+PI)/20 + sin(y*39)/20 * sin(y*20)/2 * (sin(y*20)/1) * 5+0.5;
+    return x;
+}
+void checkLastResponse(){
+    if(lastMessage < (millis()-4000)){
+        simulating = true;
+        stopFans = false;
+        lastResponse = lastMessage;
+        /*Serial.print(lastResponse);
+        Serial.print(" last respones::last message ");
+        Serial.print(lastMessage); */
+    }
+}
+void checkstopFans(){
+    if(stopFans){
+        turn((BTS7960::Direction)1, 0);
+    }
+    
+}
+void checkSimulating(){
+    if (simulating && !stopFans)
+    {
+        float result;
+        float adjusted;
+        float remap;
+        ++counter;
+        adjusted = counter%((int)(PI*200));
+        if (adjusted != 0){
+            adjusted = adjusted/100;
+        }
+        result = sinusCurveforSimulating(adjusted);
+        if(result < 0.5){
+            result = 0.5;
+        }
+        result = result*100;
+        
+        remap = map(result, 50,70,0,255);
+        turn((BTS7960::Direction)1, remap);
+        //fanController.Turn((BTS7960::Direction)1, 163);
+        statusinfo();
+        /*Serial.print(" SineCurveCounter: ");
+        Serial.print(counter);
+        Serial.print(" SineCurveAdjust: ");
+        Serial.print(adjusted);
+        Serial.print(" SineCurveResult: ");
+        Serial.print(result);
+        Serial.print(" SineCurveRemap: ");
+        Serial.println(remap);*/
+
+        delay(150);                             // plus the 100 delay from loop = 250 aka 0.25s
+    }
+}
+
+void loop_deletetowork() {
+    loopDelta = millis() - lastMillis;
+    lastMillis = millis();
+    drd->loop();
+    cli_loop();
+    #ifdef USE_WIFI
+        OscWiFi.update();
+    #endif
+    checkLastResponse();
+    checkSimulating();
+    checkstopFans();
+    delay(100);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,360 +1,185 @@
-#include <Arduino.h>
-#include "settings.h"
+// This sketch is a companion to the Bluetooth LE for iOS, tvOS and Android plugin for Unity3D.
+// It is the hardware side of the StartingExample.
 
-#include <SimpleCLI.h>
+// The URL to the asset on the asset store is:
+// https://assetstore.unity.com/packages/tools/network/bluetooth-le-for-ios-tvos-and-android-26661
 
-#include "BTS7960.h"
+// This sketch simply advertises as ledbtn and has a single service with 2 characteristics.
+// The ledUUID characteristic is used to turn the LED on and off. Writing 0 turns it off and 1 turns it on.
+// The buttonUUID characteristic can be read or subscribe to. When the button is down the characteristic
+// value is 1. When the button is up the value is 0.
 
-#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
+// This sketch was written for the Hiletgo ESP32 dev board found here:
+// https://www.amazon.com/HiLetgo-ESP-WROOM-32-Development-Microcontroller-Integrated/dp/B0718T232Z/ref=sr_1_3?keywords=hiletgo&qid=1570209988&sr=8-3
 
-#include <ArduinoOSCWiFi.h>
+// Many other ESP32 devices will work fine.
+#include "Arduino.h"
+#include "BLEDevice.h"
+#include "BLE2902.h"
 
-#include <ESP_DoubleResetDetector.h>      //https://github.com/khoih-prog/ESP_DoubleResetDetector
-// Number of seconds after reset during which a 
-// subseqent reset will be considered a double reset.
+// pin 2 on the Hiletgo
+// (can be turned on/off from the iPhone app)
+const uint32_t led = 2;
 
-DoubleResetDetector* drd;
-bool DRD_Detected = false;
+// pin 5 on the RGB shield is button 1
+// (button press will be shown on the iPhone app)
+const uint32_t button = 13;
 
-BTS7960 fanController(D_OUT_ENABLE_FAN, PWM_OUT_LPWM, PWM_OUT_RPWM);
+static BLEUUID serviceUUID("A9E90000-194C-4523-A473-5FDF36AA4D20");
+static BLEUUID ledUUID("A9E90001-194C-4523-A473-5FDF36AA4D20");
+static BLEUUID buttonUUID("A9E90002-194C-4523-A473-5FDF36AA4D20");
 
-SimpleCLI cli;
-Command cmdSpeed;
-Command cmdDirection;
-Command cmdSimulate;
-Command cmdstopFans;
-bool simulating = true;
-bool stopFans = true;
-int counter = 0;
-unsigned long lastMessage = 0;
-unsigned long lastResponse = 0;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
 
-unsigned loopDelta = 0;
-unsigned long lastMillis = 0;
+bool lastButtonState = false;
 
-void turn(BTS7960::Direction direction, uint8_t speed)
+BLEServer* pServer = 0;
+BLECharacteristic* pCharacteristicCommand = 0;
+BLECharacteristic* pCharacteristicData = 0;
+
+class BTServerCallbacks : public BLEServerCallbacks
 {
-    fanController.Turn(direction, speed);
-    /*Serial.print("Direction: ");
-    Serial.print((int) fanController.getDirection());
-    Serial.print(" Speed: ");
-    Serial.print(fanController.getPwmValue());
-    Serial.print(" Simulate: ");
-    Serial.println(simulating);*/
-}
-void statusinfo(){
-    Serial.print(" Direction: ");
-    Serial.print((int) fanController.getDirection());
-    Serial.print(" Speed: ");
-    Serial.print(fanController.getPwmValue());
-    Serial.print(" Simulate: ");
-    Serial.print(simulating);
-    Serial.print(" Stop Fans: ");
-    Serial.println(stopFans);
-}
-
-void wifi_setup()
+    void onConnect(BLEServer* pServer)
 {
-    // WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
-    // it is a good practice to make sure your code sets wifi mode how you want it.
+    Serial.println("Connected...");
+    deviceConnected = true;
+};
 
-    // put your setup code here, to run once:
-    
-    // pinMode(BTN_WIFI_RESET, INPUT_PULLUP);
-    delay(100);
-    int countDown = 10 * 1000;
-    //WiFiManager, Local intialization. Once its business is done, there is no need to keep it around
-    WiFiManager wm;
-    wm.setHostname(hostName.c_str());
-    if(DRD_Detected)
-    {
-        wm.resetSettings();
-        delay(100);
-    }
+void onDisconnect(BLEServer* pServer)
+{
+    Serial.println("Disconnected...");
+    deviceConnected = false;
 
-    bool res;
-    Serial.println("AutoConnect");
-    res = wm.autoConnect(configApSSID.c_str(), configApPW.c_str()); // password protected ap
-    //res = wm.autoConnect("RATATATATA", "12345678");
-    // reset settings - wipe stored credentials for testing
-    // these are stored by the esp library
-    // wm.resetSettings();
-
-    // Automatically connect using saved credentials,
-    // if connection fails, it starts an access point with the specified name ( "AutoConnectAP"),
-    // if empty will auto generate SSID, if password is blank it will be anonymous AP (wm.autoConnect())
-    // then goes into a blocking loop awaiting configuration and will return success result
+    // don't leave the led on if they disconnect
+    digitalWrite(led, LOW);
+}
+};
 
 
-    if(!res) {
-        Serial.println("Failed to connect");
-        // ESP.restart();
-    } 
-    else {
-        //if you get here you have connected to the WiFi    
-        Serial.println("connected...yeey :)");
-    }
-    Serial.println();
+class BTCallbacks : public BLECharacteristicCallbacks
+{
+    void onRead(BLECharacteristic* pCharacteristic)
+{
 }
 
-void cliErrorCallback(cmd_error* errorPtr) {
-    CommandError e(errorPtr);
+void onWrite(BLECharacteristic* pCharacteristic)
+{
+    uint8_t* data = pCharacteristic->getData();
+    int len = pCharacteristic->getValue().empty() ? 0 : pCharacteristic->getValue().length();
 
-    Serial.println("ERROR: " + e.toString());
-
-    if (e.hasCommand()) {
-        Serial.println("Did you mean? " + e.getCommand().toString());
-    } else {
-        Serial.println(cli.toString());
-    }
-}
-
-void cliSpeedCallback(cmd* cmdPtr) {
-    Command cmd(cmdPtr);
-
-    Argument argSpeed = cmd.getArgument("value");
-    if(argSpeed.isSet())
+    if (len > 0)
     {
-        String speedString = argSpeed.getValue();
-        int speed = speedString.toInt();
-        if(speed >= 0 && speed < 256)
+        // if the first byte is 0x01 / on / true
+        if (data[0] == 0x01)
         {
-            turn(fanController.getDirection(), speed);
+            digitalWrite(led, HIGH);
+            Serial.println("ON");
+        }
+        else
+        {
+            digitalWrite(led, LOW);
+            Serial.println("OFF");
         }
     }
 }
+};
 
-void cliDirectionCallback(cmd* cmdPtr) {
-    Command cmd(cmdPtr);
+// debounce time (in ms)
+int debounce_time = 10;
 
-    Argument argDirection   = cmd.getArgument("value");
-    if(argDirection.isSet())
+// maximum debounce timeout (in ms)
+int debounce_timeout = 100;
+
+void BluetoothStartAdvertising()
+{
+    if (pServer != 0)
     {
-        String directionString = argDirection.getValue();
-        int direction          = directionString.toInt();
-        if(direction >= 0 && direction < 3)
-        {
-            turn((BTS7960::Direction) direction, fanController.getPwmValue());
-        }
+        BLEAdvertising* pAdvertising = pServer->getAdvertising();
+        pAdvertising->start();
     }
 }
 
-void cliSimulationCallback(cmd* cmdPtr) {
-    Command cmd(cmdPtr);
-
-    Argument argSimulation   = cmd.getArgument("value");
-    if(argSimulation.isSet())
+void BluetoothStopAdvertising()
+{
+    if (pServer != 0)
     {
-        String simulateString = argSimulation.getValue();
-        int simulate          = simulateString.toInt(); 
-        if(simulate >= 0 && simulate < 2)
-        {
-            simulating = simulate;
-        }
-    }
-}
-void clistopFansCallback(cmd* cmdPtr) {
-    Command cmd(cmdPtr);
-
-    Argument argSimulation   = cmd.getArgument("value");
-    if(argSimulation.isSet())
-    {
-        String stopFansString = argSimulation.getValue();
-        int fanStop          = stopFansString.toInt(); 
-        if(fanStop >= 0 && fanStop < 2)
-        {
-            stopFans = fanStop;
-        }
+        BLEAdvertising* pAdvertising = pServer->getAdvertising();
+        pAdvertising->stop();
     }
 }
 
-void cli_loop()
+void setup()
 {
-    if (Serial.available())
-    {
-        String input = Serial.readStringUntil('\n');
-        Serial.print("# ");
-        Serial.println(input);
-        cli.parse(input);
-    }
-}
-
-void cli_setup()
-{
-    cmdSpeed = cli.addCmd("s/peed", cliSpeedCallback);
-    cmdSpeed.addPosArg("value");
-
-    cmdDirection = cli.addCmd("d/irection", cliDirectionCallback);
-    cmdDirection.addPosArg("value");
-
-    cmdSimulate = cli.addCmd("s/imulate", cliSimulationCallback);
-    cmdSimulate.addPosArg("value");
-
-    cmdstopFans = cli.addCmd("s/topFans", clistopFansCallback);
-    cmdstopFans.addPosArg("value");
-  
-    cli.setOnError(cliErrorCallback);
-
-}
-
-void oscReply(const String &remoteAddress)
-{
-    Serial.print(remoteAddress);
-    statusinfo();
-    lastMessage = millis();
-    //OscWiFi.send(remoteAddress.c_str(), OSC_SEND_PORT, "/fan/speed/state", (int)fanController.getPwmValue());
-    //OscWiFi.send(remoteAddress.c_str(), OSC_SEND_PORT, "/fan/direction/state", (int)fanController.getDirection());
-    OscWiFi.send(remoteAddress.c_str(), OSC_SEND_PORT, "/fan/connected/state", true);
-}
-
-void oscSpeedCallback(const OscMessage& m)
-{
-    if(m.isInt32(0))
-    {
-        int val = m.arg<int>(0);
-        if(val >= 0 && val < 256)
-        {
-            turn(fanController.getDirection(), val);
-        }
-        oscReply(m.remoteIP());   
-    }
-}
-
-void oscDirectionCallback(const OscMessage& m)
-{
-    if(m.isInt32(0))
-    {
-        int val = m.arg<int>(0);
-        if(val >= 0 && val < 3)
-        {
-            turn((BTS7960::Direction)val, fanController.getPwmValue());
-        }
-        oscReply(m.remoteIP());
-    }
-}
-void oscSimulationCallback(const OscMessage& m)
-{
-    if(m.isInt32(0))
-    {
-        int val = m.arg<int>(0);
-        if(val >= 0 && val < 2)
-        {
-            simulating = val;
-        }
-        oscReply(m.remoteIP());
-    }
-}
-void oscStopFansCallback(const OscMessage& m)
-{
-    if(m.isInt32(0))
-    {
-        int val = m.arg<int>(0);
-        if(val >= 0 && val < 2)
-        {
-            stopFans = val;
-        }
-        oscReply(m.remoteIP());
-    }
-}
-
-void osc_setup()
-{
-    OscWiFi.subscribe(OSC_LISTENER_PORT, "/fan/speed/set", oscSpeedCallback);
-    OscWiFi.subscribe(OSC_LISTENER_PORT, "/fan/direction/set", oscDirectionCallback);
-    OscWiFi.subscribe(OSC_LISTENER_PORT, "/fan/simulate/set", oscSimulationCallback);
-    OscWiFi.subscribe(OSC_LISTENER_PORT, "/fan/stopFans/set", oscStopFansCallback);
-}
-
-void setup() {
     Serial.begin(9600);
 
-    drd = new DoubleResetDetector(DRD_TIMEOUT, DRD_ADDRESS);
-    if (drd->detectDoubleReset()) 
-    {
-        Serial.println("Double Reset Detected");
-        DRD_Detected = true;
-    } 
+    // led turned on/off from the iPhone app
+    pinMode(led, OUTPUT);
 
-    // put your setup code here, to run once:
-    // pinMode(BTN_WIFI_RESET, INPUT_PULLUP);
-    pinMode(LED_BUILTIN, OUTPUT);
-    //pinMode(A_IN_RPWM, INPUT_PULLDOWN);
-    Serial.println("I am " + hostName);
-    cli_setup();
-    #ifdef USE_WIFI
-      wifi_setup();
-      osc_setup();
-    #endif
-    Serial.println("Setup done");
-    lastMillis = millis();
-    lastResponse = millis();
+    // button press will be shown on the iPhone app)
+    pinMode(button, INPUT);
+
+    BLEDevice::init("ledbtn");
+    // BLEDevice::setCustomGattsHandler(my_gatts_event_handler);
+    // BLEDevice::setCustomGattcHandler(my_gattc_event_handler);
+
+    pServer = BLEDevice::createServer();
+    BLEService* pService = pServer->createService(serviceUUID);
+    pServer->setCallbacks(new BTServerCallbacks());
+
+    pCharacteristicCommand = pService->createCharacteristic(
+        buttonUUID,
+        BLECharacteristic::PROPERTY_READ |
+            BLECharacteristic::PROPERTY_WRITE |
+            BLECharacteristic::PROPERTY_NOTIFY);
+
+    pCharacteristicCommand->setCallbacks(new BTCallbacks());
+    pCharacteristicCommand->setValue("");
+    pCharacteristicCommand->addDescriptor(new BLE2902());
+
+    pCharacteristicData = pService->createCharacteristic(
+        ledUUID,
+        BLECharacteristic::PROPERTY_READ |
+            BLECharacteristic::PROPERTY_WRITE |
+            BLECharacteristic::PROPERTY_NOTIFY);
+
+    pCharacteristicData->setCallbacks(new BTCallbacks());
+    pCharacteristicData->setValue("");
+    pCharacteristicData->addDescriptor(new BLE2902());
+
+    pService->start();
+    BluetoothStartAdvertising();
 }
 
-float sinusCurveforSimulating(float y){
-    float x;
-    x = sin(y*20)/16 + sin(y*25+PI)/20 + sin(y*28+PI)/20 + sin(y*39)/20 * sin(y*20)/2 * (sin(y*20)/1) * 5+0.5;
-    return x;
-}
-void checkLastResponse(){
-    if(lastMessage < (millis()-4000)){
-        simulating = true;
-        stopFans = false;
-        lastResponse = lastMessage;
-        /*Serial.print(lastResponse);
-        Serial.print(" last respones::last message ");
-        Serial.print(lastMessage); */
-    }
-}
-void checkstopFans(){
-    if(stopFans){
-        turn((BTS7960::Direction)1, 0);
-    }
-    
-}
-void checkSimulating(){
-    if (simulating && !stopFans)
+void loop()
+{
+    if (pServer != 0)
     {
-        float result;
-        float adjusted;
-        float remap;
-        ++counter;
-        adjusted = counter%((int)(PI*200));
-        if (adjusted != 0){
-            adjusted = adjusted/100;
+        // disconnecting
+        if (!deviceConnected && oldDeviceConnected)
+        {
+            delay(500);                  // give the bluetooth stack the chance to get things ready
+            pServer->startAdvertising(); // restart advertising
+            Serial.println("start advertising");
+            oldDeviceConnected = deviceConnected;
         }
-        result = sinusCurveforSimulating(adjusted);
-        if(result < 0.5){
-            result = 0.5;
+
+        // connecting
+        if (deviceConnected && !oldDeviceConnected)
+        {
+            oldDeviceConnected = deviceConnected;
         }
-        result = result*100;
+
+        uint8_t buttonState = digitalRead(button);
         
-        remap = map(result, 50,70,0,255);
-        turn((BTS7960::Direction)1, remap);
-        //fanController.Turn((BTS7960::Direction)1, 163);
-        statusinfo();
-        /*Serial.print(" SineCurveCounter: ");
-        Serial.print(counter);
-        Serial.print(" SineCurveAdjust: ");
-        Serial.print(adjusted);
-        Serial.print(" SineCurveResult: ");
-        Serial.print(result);
-        Serial.print(" SineCurveRemap: ");
-        Serial.println(remap);*/
+        if (deviceConnected && pCharacteristicCommand != 0 && buttonState != lastButtonState)
+        {
+            lastButtonState = buttonState;
 
-        delay(150);                             // plus the 100 delay from loop = 250 aka 0.25s
+            uint8_t packet[1];
+            packet[0] = buttonState == HIGH ? 0x00 : 0x01;
+            pCharacteristicCommand->setValue(packet, 1);
+            pCharacteristicCommand->notify();
+        }
     }
-}
-
-void loop() {
-    loopDelta = millis() - lastMillis;
-    lastMillis = millis();
-    drd->loop();
-    cli_loop();
-    #ifdef USE_WIFI
-        OscWiFi.update();
-    #endif
-    checkLastResponse();
-    checkSimulating();
-    checkstopFans();
-    delay(100);
 }
